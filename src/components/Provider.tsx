a7703e7185161ecf371441c8a23fb1392ed4eba7
kdh379 <ehgus37625@naver.com>
2024-01-22 21:57:32 +0900

    build: shadcn, twin.macro 추가

--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -1,17 +1,12 @@
-import { ChakraProvider, Flex } from "@chakra-ui/react";
 import { Inter } from "next/font/google";
 import { PropsWithChildren } from "react";
-
-import theme from "@/styles/theme";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Providers(props: PropsWithChildren) {
   return (
-    <ChakraProvider theme={theme}>
-      <Flex className={inter.className}>
-        {props.children}
-      </Flex>
-    </ChakraProvider>
+    <div className={inter.className}>
+      {props.children}
+    </div>
   );
 }
