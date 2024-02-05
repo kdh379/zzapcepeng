010473f5a37eed12f451e8d2e05517ad6f72557a
kdh379 <kdh379@innorules.com>
2024-02-05 17:56:39 +0900

    chore(header): 반응형 css

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -37,43 +37,45 @@ export default function Header() {
   });
 
   return <header className="flex items-center mx-auto py-4">
-    <nav className="flex items-center space-x-8">
-      <Link href="/">
-        <span className="text-2xl text-foreground font-extrabold">Lcalc</span>
-      </Link>
-      <ul className="flex items-center gap-x-4 ml-4">
-        <li>
-          <Link href="/engraving">
-            <Button
-              type="button"
-              className={cn("text-muted-foreground", pathname === "/engraving" && "text-primary")}
-              variant="ghost"
-              size="sm"
-            >
+    <Link href="/">
+      <span className="text-2xl text-foreground font-extrabold">Lcalc</span>
+    </Link>
+    <div className="hidden w-full items-center tablet:flex">
+      <nav className="flex items-center mx-8">
+        <ul className="flex items-center gap-x-4 ml-4">
+          <li>
+            <Link href="/engraving">
+              <Button
+                type="button"
+                className={cn("text-muted-foreground", pathname === "/engraving" && "text-primary")}
+                variant="ghost"
+                size="sm"
+              >
               각인 계산기
-            </Button>
-          </Link>
-        </li>
-      </ul>
-    </nav>
-    <div className="flex items-center ml-auto gap-x-4">
-      <Form {...form}>
-        <FormField
-          name="apiKey"
-          control={form.control}
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <Input type="text" className="w-64" placeholder="API 키를 입력해주세요." {...field} />
-              </FormControl>
-            </FormItem>
-          )}
-        />
-        <Button type="submit" variant="default" size="sm">
-          <LogInIcon className="size-4" />
-        </Button>
-      </Form>
-      <ToggleTheme />
+              </Button>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="flex items-center ml-auto gap-x-4">
+        <Form {...form}>
+          <FormField
+            name="apiKey"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input type="text" className="w-64" placeholder="API 키를 입력해주세요." {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <Button type="submit" variant="default" size="sm">
+            <LogInIcon className="size-4" />
+          </Button>
+        </Form>
+        <ToggleTheme />
+      </div>
     </div>
   </header>;
 }
